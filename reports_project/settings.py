392547c1fbccabc894d5d1d643faf15865a9008e
saswatpanda001 <skpandatech@yahoo.com>
2023-10-16 22:38:57 +0530

    second

--- reports_project/settings.py
+++ reports_project/settings.py
@@ -20,7 +20,8 @@ SECRET_KEY = "django-insecure-bhlo$x!k%4rj3lqli(#jc1jt0#tiu(&t6in-+h+g6i6%%fq_oy
 
 
 DEBUG = True
-ALLOWED_HOSTS = ["mycircles.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["mycircles.herokuapp.com", "localhost", "0.0.0.0","127.0.0.1","xtraas.com","3.96.222.240"]
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
